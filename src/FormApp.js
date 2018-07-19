1a9a11ead2cae7bdbcd9320da9a9d27deb470b26
Hirotoshin <hirotoshin0211@gmail.com>
2018-07-20 01:28:44 +0900

    [Add]リクエスト時メッセージが送れるようにした

--- src/FormApp.js
+++ src/FormApp.js
@@ -1,4 +1,5 @@
 import  React, {Component} from  'react';
+import  request from "superagent";
 
 export  class FormApp extends Component{
 
@@ -6,7 +7,7 @@ export  class FormApp extends Component{
         super(props);
         this.state = {
             value:'',
-            message:''
+            messages:[]
 
         };
     }
@@ -15,20 +16,23 @@ export  class FormApp extends Component{
            value
        });
 
-
-
-        console.log('handleInput calded');
-        console.log(this.props.name);
     }
     send(){
        const {value} = this.state;
-        this.setState({
-            value:"",
-            message:value
-        });
 
+        request
+            .post("http://127.0.0.1:5000/message")
+            .set("Content-Type","application/json")
+            .send({
+                message:value
+            })
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    messages:res.body.Allmessage
+                })
+            })
 
-        console.log('send called');
     }
 
     render(){
@@ -36,7 +40,11 @@ export  class FormApp extends Component{
             <div>
                 <input type ="text" value={this.state.value} onChange={this.handleInput.bind(this)}/>
                 <button onClick={this.send.bind(this)}>SEND</button>
-                <div>{this.state.message}</div>
+                {this.state.messages.map((message) =>{
+                    return <div>{message}</div>
+                }
+                )
+                }
                 </div>
         );
 
